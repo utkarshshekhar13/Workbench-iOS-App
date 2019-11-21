be8924e025199caa0ad77fa769ba08fa15f5f1e4
utkarshshekhar13 <utki.619@gmail.com>
2019-11-21 13:04:56 +0530

    Firebase Login/SignUp
    Signed-off-by: utkarshshekhar13 <utki.619@gmail.com>

--- Workbench iOS App/ViewController.swift
+++ Workbench iOS App/ViewController.swift
@@ -7,14 +7,53 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //signUpWithEmailAndPassword()
+        signInWithEmailAndPassword()
+        //sendPasswordResetLink()
     }
-
-
+    
+    func signUpWithEmailAndPassword(){
+        Auth.auth().createUser(withEmail: "utki.619@gmail.com", password: "password"){ AuthResultCallback, error in
+            
+            guard let user = AuthResultCallback?.user, error == nil else {
+                print(error!.localizedDescription)
+                return
+            }
+            print("\(user.email!) created")
+            
+        }
+    }
+    
+    func signInWithEmailAndPassword(){
+        Auth.auth().signIn(withEmail: "utki.619@gmail.com", password: "password"){ authResult,error in
+            
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            }
+            
+            print("SignIn Successfull")
+        }
+    }
+    
+    
+    func sendPasswordResetLink(){
+        Auth.auth().sendPasswordReset(withEmail: "utki.619@gmail.com"){ error in
+            if let error = error {
+                print(error.localizedDescription)
+            }
+            
+            print("Password Reset Main has been sent to user's mailId")
+            
+        }
+    }
+    
 }
 
