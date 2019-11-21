2f4ba5bc5640e282614051c2b406a011520d08cd
utkarshshekhar13 <utkarsh.shekhar@sysforetech.local>
2019-11-21 13:27:37 +0530

    Logged Out Version
    Signed-off-by: utkarshshekhar13 <utkarsh.shekhar@sysforetech.local>

--- Workbench iOS App/ViewController.swift
+++ Workbench iOS App/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         //signUpWithEmailAndPassword()
-        signInWithEmailAndPassword()
+        //signInWithEmailAndPassword()
         //sendPasswordResetLink()
     }
     
